9c567bb995355f96016160e002ca9d3861597344
alex <alejandro_n@outlook.com>
2022-02-06 14:25:53 -0600

    added response to the API

--- ScrapperAPI/app.py
+++ ScrapperAPI/app.py
@@ -12,7 +12,7 @@ def mercadolibre():
         title_list, images_url_list, url_list, prices_list = allproducts(data['product'])
     else:
          title_list, images_url_list, url_list, prices_list = limitedProducts(data['product'], data['limit'])
-    return return Response(headers={'Access-Control-Allow-Origin':'*'}), jsonify({"datos":{"titulo":title_list, "img": images_url_list, "url":url_list, "precios": prices_list}})
+    return Response(headers={'Access-Control-Allow-Origin':'*'}), jsonify({"datos":{"titulo":title_list, "img": images_url_list, "url":url_list, "precios": prices_list}})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
